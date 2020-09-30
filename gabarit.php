372ca71a25de9272c7a0627154dfef0fb35cbcca
jlebihan <johan.lebihan@gmail.com>
2020-09-30 17:01:49 +0200

    version2

---
+++ gabarit.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="fr">
+ <head>
+ <meta charset="UTF-8" />
+ <link rel="stylesheet" href="style.css" />
+ <title><?= $titre ?></title> <!-- Élément spécifique -->
+ </head>
+ <body>
+ <div id="global">
+ <div id="contenu">
+ <?= $contenu ?> <!-- Élément spécifique -->
+ </div>
+ <footer id="piedBlog"> Blog réalisé avec PHP, HTML5 et CSS.
+ </footer>
+ </div> <!-- #global -->
+ </body>
+</html>
